de2fa94ebb38a4c01d41cf61a7f886bdcb7dc75d
Daniel Merchan <demerchanme@gmail.com>
2024-11-18 12:22:16 -0500

    actualizacion fastapi

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,23 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return[1,2,3,]
+
+@app.get('/contacts')
+def get_list():
+    return {'name':'Platzi'}
+
+
+@app.get('/html', response_class = HTMLResponse)
+def get_list():
+    return """
+        <h1> Hola soy un codigo HTML </h1>
+    """
 
 def run():
     store.get_categories()
